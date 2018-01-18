f1c72967dad7637ac9ec203adbb5a899ee56dcc2
Charlie Hsieh <charlieofficial@gmail.com>
2018-01-18 20:23:55 +0800

    refine code

--- stress_ab.sh
+++ stress_ab.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+URL="${1}"
 for((i=1;;i++)); do
   echo "------------------------"
-  echo "ab tool round $i"
+  echo "Stress tool (ab) Round  $i on $URL"
   echo "------------------------"
-  ab -c 100 -t 86400 $1
+  ab -c 100 -t 86400 $URL
+  echo
+
   RC=$?
   if [[ $RC -eq 1 ]]; then
     echo "terminated...."
+    echo
     exit
   fi
-  echo
 done
