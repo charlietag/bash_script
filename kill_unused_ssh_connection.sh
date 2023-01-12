8dcd5e92eb4541b43972523d2db1d741081b7b7a
Charlie Hsieh <charlieofficial@gmail.com>
2023-01-13 03:39:36 +0800

    xargs -i argv is deprecated

--- kill_unused_ssh_connection.sh
+++ kill_unused_ssh_connection.sh
@@ -1,1 +1,1 @@
-netstat -palunt |grep -i est | awk '{print $7}'| cut -d'/' -f1 |xargs -i bash -c "ps aux |grep sshd |grep {}|grep -v grep" | head -n -1 | awk '{print $2}' |xargs -i kill {}
+netstat -palunt |grep -i est | awk '{print $7}'| cut -d'/' -f1 |xargs -I{} bash -c "ps aux |grep sshd |grep {}|grep -v grep" | head -n -1 | awk '{print $2}' |xargs -I{} kill {}
